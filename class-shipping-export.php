649fbb921948694baa367b7a5012a73b774f968c
Pippin Williamson <pippin@pippinsplugins.com>
2013-04-23 14:11:50 -0500

    Improved unshipped orders export

--- class-shipping-export.php
+++ class-shipping-export.php
@@ -34,6 +34,7 @@ class EDD_Simple_Shipping_Export extends EDD_Export {
 	public function csv_cols() {
 		$cols = array(
 			'id'         => __( 'Order ID',   'edd-simple-shipping' ),
+			'date'       => __( 'Date', 'edd' ),
 			'first_name' => __( 'First Name', 'edd-simple-shipping' ),
 			'last_name'  => __( 'Last Name', 'edd-simple-shipping' ),
 			'email'      => __( 'Email', 'edd-simple-shipping' ),
@@ -42,7 +43,13 @@ class EDD_Simple_Shipping_Export extends EDD_Export {
 			'city'       => __( 'City', 'edd-simple-shipping' ),
 			'state'      => __( 'State / Province', 'edd-simple-shipping' ),
 			'zip'        => __( 'Zip / Postal Code', 'edd-simple-shipping' ),
-			'country'    => __( 'Country', 'edd-simple-shipping' )
+			'country'    => __( 'Country', 'edd-simple-shipping' ),
+			'amount'     => __( 'Amount', 'edd' ) . ' (' . html_entity_decode( edd_currency_filter( '' ) ) . ')',
+			'tax'        => __( 'Tax', 'edd' ) . ' (' . html_entity_decode( edd_currency_filter( '' ) ) . ')',
+			'gateway'    => __( 'Payment Method', 'edd' ),
+			'key'        => __( 'Purchase Key', 'edd' ),
+			'products'   => __( 'Products', 'edd' ),
+			'status'     => __( 'Payment Status', 'edd' )
 		);
 		return $cols;
 	}
@@ -72,9 +79,30 @@ class EDD_Simple_Shipping_Export extends EDD_Export {
 		if ( $payments ) {
 			foreach ( $payments as $payment ) {
 				$user_info = edd_get_payment_meta_user_info( $payment );
+				$downloads      = edd_get_payment_meta_cart_details( $payment );
+				$products       = '';
 
+				if ( $downloads ) {
+					foreach ( $downloads as $key => $download ) {
+						// Download ID
+						$id = isset( $payment_meta['cart_details'] ) ? $download['id'] : $download;
+
+						// If the download has variable prices, override the default price
+						$price_override = isset( $payment_meta['cart_details'] ) ? $download['price'] : null;
+
+						$price = edd_get_download_final_price( $id, $user_info, $price_override );
+
+						// Display the Downoad Name
+						$products .= get_the_title( $id );
+
+						if ( $key != ( count( $downloads ) -1 ) ) {
+							$products .= ' / ';
+						}
+					}
+				}
 				$data[]    = array(
 					'id'         => $payment,
+					'date'       => get_post_field( 'post_date', $payment ),
 					'first_name' => $user_info['first_name'],
 					'last_name'  => $user_info['last_name'],
 					'email'      => $user_info['email'],
@@ -83,7 +111,13 @@ class EDD_Simple_Shipping_Export extends EDD_Export {
 					'city'       => $user_info['shipping_info']['city'],
 					'state'      => $user_info['shipping_info']['state'],
 					'zip'        => $user_info['shipping_info']['zip'],
-					'country'    => $user_info['shipping_info']['country']
+					'country'    => $user_info['shipping_info']['country'],
+					'amount'     => edd_get_payment_amount( $payment ),
+					'tax'        => edd_get_payment_tax( $payment ),
+					'gateway'    => edd_get_payment_gateway( $payment ),
+					'key'        => edd_get_payment_key( $payment ),
+					'products'   => $products,
+					'status'     => get_post_field( 'post_status', $payment )
 				);
 			}
 		}
