c365a0220f838c99ac41d53afb8a24920617947e
Pippin Williamson <pippin@pippinsplugins.com>
2014-05-07 16:04:57 -0500

    Add custom FES field

--- edd-simple-shipping.php
+++ edd-simple-shipping.php
@@ -232,7 +232,7 @@ class EDD_Simple_Shipping {
 		$international = get_post_meta( $post_id, '_edd_shipping_international', true );
 ?>
 		<div id="edd_simple_shipping">
-			<script type="text/javascript">jQuery(document).ready(function($){$('#edd_enable_shipping').on('click',function(){$('#edd_simple_shipping_fields,.edd_prices_shipping').toggle();});});</script>
+			<script type="text/javascript">jQuery(document).ready(function($) {$('#edd_enable_shipping').on('click',function() {$('#edd_simple_shipping_fields,.edd_prices_shipping').toggle();});});</script>
 			<p><strong><?php _e( 'Shipping Options', 'edd-simple-shipping' ); ?></strong></p>
 			<p>
 				<label for="edd_enable_shipping">
@@ -608,8 +608,8 @@ class EDD_Simple_Shipping {
 
 		ob_start();
 ?>
-		<script type="text/javascript">var edd_global_vars; jQuery(document).ready(function($){
-			$('body').on('change', 'select[name=shipping_country],select[name=billing_country]',function(){
+		<script type="text/javascript">var edd_global_vars; jQuery(document).ready(function($) {
+			$('body').on('change', 'select[name=shipping_country],select[name=billing_country]',function() {
 
 				var billing = true;
 
@@ -625,7 +625,7 @@ class EDD_Simple_Shipping {
 
 				if( val =='US') {
 					$('#shipping_state_other').hide();$('#shipping_state_us').show();$('#shipping_state_ca').hide();
-				} else if(  val =='CA'){
+				} else if(  val =='CA') {
 					$('#shipping_state_other').hide();$('#shipping_state_us').hide();$('#shipping_state_ca').show();
 				} else {
 					$('#shipping_state_other').show();$('#shipping_state_us').hide();$('#shipping_state_ca').hide();
@@ -702,7 +702,7 @@ class EDD_Simple_Shipping {
 		            console.log(data);
 		        });
 			});
-			$('#edd_simple_shipping_show').change(function(){
+			$('#edd_simple_shipping_show').change(function() {
 				$('#edd_simple_shipping_fields_wrap').toggle();
 			});
 		});</script>
@@ -1413,3 +1413,123 @@ function edd_simple_shipping_load() {
 	$edd_simple_shipping = new EDD_Simple_Shipping();
 }
 add_action( 'plugins_loaded', 'edd_simple_shipping_load' );
+
+
+/**
+ * Register a custom FES submission form button 
+ *
+ * @since 2.0
+ *
+ * @return void
+ */
+function edd_fes_simple_shipping_field_button( $title ) {
+	if ( version_compare( fes_plugin_version, '2.2', '>=' ) ) {
+		echo  '<button class="fes-button button" data-name="edd_simple_shipping" data-type="action" title="' . esc_attr( $title ) . '">'. __( 'Shipping', 'edd-simple-shipping' ) . '</button>';
+	}
+}
+add_action( 'fes_custom_post_button', 'edd_fes_simple_shipping_field_button' );
+
+/**
+ * Setup the custom FES form field
+ *
+ * @since 2.0
+ *
+ * @return void
+ */
+function edd_fes_simple_shipping_admin_field( $field_id, $label = "", $values = array() ) {
+	if( ! isset( $values['label'] ) ) {
+		$values['label'] = __( 'Shipping', 'edd-simple-shipping' );
+	}
+
+	$values['no_css']  = true;
+	$values['is_meta'] = true;
+	$values['name']    = 'edd_simple_shipping';
+	?>
+	<li class="edd_simple_shipping">
+		<?php FES_Formbuilder_Templates::legend( $values['label'] ); ?>
+		<?php FES_Formbuilder_Templates::hidden_field( "[$field_id][input_type]", 'edd_simple_shipping' ); ?>
+		<?php FES_Formbuilder_Templates::hidden_field( "[$field_id][template]", 'edd_simple_shipping' ); ?>
+		<div class="fes-form-holder">
+			<?php FES_Formbuilder_Templates::common( $field_id, 'edd_simple_shipping', false, $values ); ?>
+		</div> <!-- .fes-form-holder -->
+	</li>
+	<?php
+}
+add_action( 'fes_admin_field_edd_simple_shipping', 'edd_fes_simple_shipping_admin_field', 10, 3 );
+
+/**
+ * Indicate that this is a custom field
+ *
+ * @since 2.0
+ *
+ * @return bool
+ */
+function edd_fes_simple_shipping_formbuilder_is_custom_field( $bool, $template_field ) {
+	if ( $bool ) {
+		return $bool;
+	} else if ( isset( $template_field['template'] ) && $template_field['template'] == 'edd_simple_shipping' ) {
+		return true;
+	} else {
+		return $bool;
+	}
+}
+add_filter( 'fes_formbuilder_custom_field', 'edd_fes_simple_shipping_formbuilder_is_custom_field', 10, 2 );
+
+/**
+ * save the input values when the submission form is submitted
+ *
+ * @since 2.0
+ *
+ * @return void
+ */
+function edd_fes_simple_shipping_save_custom_fields( $post_id ) {
+	if ( isset( $_POST ['edd_simple_shipping'] ) ) {
+		$domestic      = ! empty( $_POST ['edd_simple_shipping']['domestic'] ) ? edd_sanitize_amount( $_POST ['edd_simple_shipping']['domestic'] ) : 0;
+		$international = ! empty( $_POST ['edd_simple_shipping']['international'] ) ? edd_sanitize_amount( $_POST ['edd_simple_shipping']['international'] ) : 0;
+		update_post_meta( $post_id, '_edd_enable_shipping', '1' );
+		update_post_meta( $post_id, '_edd_shipping_domestic', $domestic );
+		update_post_meta( $post_id, '_edd_shipping_international', $international );
+	} else {
+		delete_post_meta( $post_id, '_edd_enable_shipping' );
+	}
+}
+add_action( 'fes_submission_form_save_custom_fields', 'edd_fes_simple_shipping_save_custom_fields' );
+
+/**
+ * Render our shipping fields in the submission form
+ *
+ * @since 2.0
+ *
+ * @return void
+ */
+function edd_fes_simple_shipping_field( $attr, $post_id, $type ) {
+
+	if ( isset( $attr['required'] ) && $attr['required'] == 'yes' ) {
+		$required = apply_filters( 'fes_required_class', ' edd-required-indicator', $attr );
+	}
+
+	$enabled       = get_post_meta( $post_id, '_edd_enable_shipping', true );
+	$domestic      = get_post_meta( $post_id, '_edd_shipping_domestic', true );
+	$international = get_post_meta( $post_id, '_edd_shipping_international', true );
+
+	?>
+	<style>
+	div.fes-form fieldset .fes-fields.edd_simple_shipping label { width: 100%; display:block; }
+	div.fes-form fieldset .fes-fields.edd_simple_shipping .edd-fes-shipping-fields label { width: 45%; display:inline-block; }
+	div.fes-form fieldset .fes-fields .edd-shipping-field { width: 45%; display:inline-block; }
+	</style>
+	<div class="fes-fields <?php echo sanitize_key( $attr['name']); ?>">
+		<label for="edd_simple_shipping[enabled]">
+			<input type="checkbox" name="edd_simple_shipping[enabled]" id="edd_simple_shipping[enabled]" value="1"<?php checked( '1', $enabled ); ?>/>
+			<?php _e( 'Enable Shipping', 'edd-simple-shipping' ); ?>
+		</label>
+		<div class="edd-fes-shipping-fields">
+			<label for="edd_simple_shipping[domestic]"><?php _e( 'Domestic', 'edd-simple-shipping' ); ?></label>
+			<label for="edd_simple_shipping[domestic]"><?php _e( 'International', 'edd-simple-shipping' ); ?></label>
+			<input class="edd-shipping-field textfield<?php echo esc_attr( $required ); ?>" id="edd_simple_shipping[domestic]" type="text" data-required="<?php echo $attr['required'] ?>" data-type="text" name="<?php echo esc_attr( $attr['name'] ); ?>[domestic]" placeholder="<?php echo __( 'Enter the domestic shipping charge amount', 'edd-simple-shipping' ); ?>" value="<?php echo esc_attr( $domestic ) ?>" size="10" />
+			<input class="edd-shipping-field textfield<?php echo esc_attr( $required ); ?>" id="edd_simple_shipping[international]" type="text" data-required="<?php echo $attr['required'] ?>" data-type="text" name="<?php echo esc_attr( $attr['name'] ); ?>[international]" placeholder="<?php echo __( 'Enter the international shipping charge amount', 'edd-simple-shipping' ); ?>" value="<?php echo esc_attr( $international ) ?>" size="10" />
+		</div>
+	</div> <!-- .fes-fields -->
+	<?php
+}
+add_action( 'fes_render_field_edd_simple_shipping', 'edd_fes_simple_shipping_field', 10, 3 );
